8a9e52b1dd32ceafa65c83c0912e8750714c0cc6
jcuush <jrcushing@sbcglobal.net>
2020-12-03 12:57:53 -0600

    set up app.js and index.js

---
+++ client/src/utils/API.js
@@ -0,0 +1,9 @@
+  
+import axios from "axios";
+const BASEURL = "https://randomuser.me/api/?results=200&nat=us"
+//This exports our api call so that TableContainer can use it
+export default {
+    searchName: function() {
+        return axios.get(BASEURL);
+    }
+}
